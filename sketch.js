24a17e1b0bd13289057e288e35a3361efbecd7b1
lavanya1302 <64483924+lavanya1302@users.noreply.github.com>
2020-07-14 21:17:58 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,9 +1,35 @@
-function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var plinko,particles;
+var backgroundImg,platform;
+
+var gameState = "onDiv";
+var bg = "sprites/bg1.png";
+var score = 0;
+
+var gameState="";
+function preload() {
+    backgroundImg = loadImage("sprites/bg.png");
 }
 
-function draw() {
-  background(255,255,255);  
-  drawSprites();
+function setup() {
+  createCanvas(480,800);
+  createSprite(400, 200, 50, 50);
+  div=new Division(100,100,50,50);
+
 }
+    
+
+
+function draw() {
+  background(255,255,255); 
+  
+div.display();
+  drawSprites();
+    
+}
+
